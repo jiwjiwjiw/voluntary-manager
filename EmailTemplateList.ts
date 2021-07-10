929be6aee07429a896000f4a8f58157b33b1763d
Jean Iwanowski <jean.iwanowski@edelweissloisirs.ch>
2021-07-10 23:33:59 +0200

    add html generation with respect of conditions add placehoder replacement

--- EmailTemplateList.ts
+++ EmailTemplateList.ts
@@ -16,7 +16,8 @@ class EmailTemplateList {
     private isTemplate(sheet: GoogleAppsScript.Spreadsheet.Sheet): boolean {
         const maxColumnsOk = sheet.getMaxColumns() === 2
         const firstColumnNameOk = sheet.getRange(1, 1).getValue() === "Type"
-        const secondColumnNameOk = sheet.getRange(1, 2).getValue() === "Texte"
+        const secondColumnNameOk = sheet.getRange(1, 2).getValue() === "Condition"
+        const thirdColumnNameOk = sheet.getRange(1, 3).getValue() === "Texte"
         return maxColumnsOk && firstColumnNameOk && secondColumnNameOk
     }
 }
