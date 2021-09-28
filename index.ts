6c0dcb1ca20a77bde6f5586f4a5a8ead4184d08d
Jean Iwanowski <jean.iwanowski@edelweissloisirs.ch>
2021-09-28 23:11:05 +0200

    add menu elements to send meeting agenda and generate meeting minutes

--- index.ts
+++ index.ts
@@ -4,8 +4,21 @@ validationHandler.add(new Validation("Sujets", "C2:C", "Personnes", "A2:A"))
 validationHandler.add(new Validation("Réunions", "D2:D", "Personnes", "A2:A"))
 
 function onOpen() {
-}
+    let ui = SpreadsheetApp.getUi();
+    ui.createMenu('Réunion')
+      .addItem('Envoi ordre du jour', 'onSendAgenda')
+      .addItem('Génération procès-verbal', 'onGenerateMeetingMinutes')
+      .addToUi();
+  }
 
 function onEdit(e) {
     validationHandler.update(e.range)
+}
+
+function onGenerateMeetingMinutes() {
+    
+}
+
+function onSendAgenda() {
+    
 }
