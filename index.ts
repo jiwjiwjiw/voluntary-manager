234904c4f4675a5e3fa9a1f5056a13ed17d974f6
Jean Iwanowski <jean.iwanowski@edelweissloisirs.ch>
2021-10-14 13:52:54 +0200

    attempt to add new version of generated PDF

--- index.ts
+++ index.ts
@@ -21,6 +21,8 @@ function onSendMeetingAgenda() {
 }
 
 function onGenerateMeetingMinutes() {
+
+  // detect meeting for which meeting minutes must be generated
   const sheetName = SpreadsheetApp.getActiveSheet().getSheetName()
   const currentRow = SpreadsheetApp.getCurrentCell().getRow()
   let date: Date
@@ -33,6 +35,7 @@ function onGenerateMeetingMinutes() {
       return
   }
 
+  // get meeting data
   const parser = new Parser
   parser.parse()
   const meeting = parser.meetings.find(x => x.date.getTime() === date.getTime() && x.subject === subject)
@@ -41,17 +44,19 @@ function onGenerateMeetingMinutes() {
     return
   }
 
+  // delete current file in spreadsheet if existing
+  // let sheet = SpreadsheetApp.getActiveSheet();
+  // let currentId = sheet.getRange('H' + currentRow).getValue().match(/[-\w]{25,}(?!.*[-\w]{25,})/)
+  // if (currentId) DriveApp.getFileById(currentId).setTrashed(true)
 
-  let sheet = SpreadsheetApp.getActiveSheet();
-  let currentId = sheet.getRange('H' + currentRow).getValue().match(/[-\w]{25,}(?!.*[-\w]{25,})/)
-  if (currentId) DriveApp.getFileById(currentId).setTrashed(true)
+  // create new file from template
   let templateFile = DriveApp.getFileById('1us4ErUoIChWcHvfM4tNDHfMhWb0yQrSw6ajV4gulu1c');
   let destinationFolder = DriveApp.getFolderById('1jWBay2PXXePEtcmBd6A_mYQZ-cqhZzDw');
-  let newFile = templateFile.makeCopy(`Réunion du ${meeting.date.toLocaleDateString()}`, destinationFolder);
+  const fileName = `Réunion du ${meeting.date.toLocaleDateString()}`
+  let newFile = templateFile.makeCopy(fileName, destinationFolder);
   var fileToEdit = DocumentApp.openById(newFile.getId());
 
-  const documentUrl = `https://docs.google.com/document/d/${newFile.getId()}/edit`;
-  SpreadsheetApp.getActiveSheet().getRange('H' + currentRow).setValue(documentUrl)
+  // replace placeholders in file
   let docBody = fileToEdit.getBody();
   let now = new Date()
   docBody.replaceText('%OBJET%', meeting.subject); 
@@ -64,9 +69,25 @@ function onGenerateMeetingMinutes() {
   replacePlaceholderByList('%EXCUSES%', meeting.excused, x => `${x.name} (${x.acronym})`);
   replacePlaceholderByList('%ABSENTS%', meeting.missing, x => `${x.name} (${x.acronym})`);
 
+  // generate pdf
   fileToEdit.saveAndClose()
   let docblob = fileToEdit.getAs('application/pdf')
-  DriveApp.createFile(docblob)
+  let pdfFile: GoogleAppsScript.Drive.File
+  if(destinationFolder.getFilesByName(fileName).hasNext()) {
+    pdfFile = destinationFolder.getFilesByName(fileName).next()
+    Drive.Files.update({
+      title: pdfFile.getName(), mimeType: pdfFile.getMimeType()
+    }, pdfFile.getId(), docblob);
+  } else {
+    pdfFile = destinationFolder.createFile(docblob)
+  }
+
+    // add link to new document in spreadsheet
+    const documentUrl = `https://docs.google.com/document/d/${pdfFile.getId()}/edit`
+    SpreadsheetApp.getActiveSheet().getRange('H' + currentRow).setValue(documentUrl)
+
+  // delete doc file
+  newFile.setTrashed(true)
 
   function replacePlaceholderByList(placeholder: string, list: any[], transform: Function) {
     let element = docBody
