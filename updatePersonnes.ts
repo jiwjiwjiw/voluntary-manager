7a43b2e732044b0fc25f93f3845db3420a2a3208
Jean Iwanowski <jean.iwanowski@edelweissloisirs.ch>
2021-10-26 16:32:44 +0200

    add missing files

---
+++ updatePersonnes.ts
@@ -0,0 +1,9 @@
+function updatePersonnes() {
+    let personnesSheet = SpreadsheetApp.getActive().getSheetByName('personnes') 
+    let personnesRange = personnesSheet.getRange('A2:F')
+    let personnesData = personnesRange.getValues()
+    let output = personnesData.map(x => (x[0] && x[1]) ? [x[1] + ' ' + x[0]] : [''])
+    let outputRange =  personnesSheet.getRange('C2:C') 
+    outputRange.setValues(output) 
+  }
+  
